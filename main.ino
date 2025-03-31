75ebb7bc27efe57820cc805546d0cc32a8ae596e
schnoggy <123883495+schnoggy@users.noreply.github.com>
2025-03-30 22:35:51 -0400

    Create main.ino

---
+++ main.ino
@@ -0,0 +1,6 @@
+# Design paradigm:
+# 2 inputs, one for pitch, one for volume
+# 1 output, sound
+# if there's time, add knob or other input to be able to control another parameter such as modulating something such as a reverb effect
+# show the math for how the electric fields work and where they go
+# use libraries whree necessary
